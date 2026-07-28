ce04117dd527231dba6483bf5ab2805ef5d6cf9c
maciejpikulski1402 <maciej.pikulski1402@gmail.com>
2026-07-28 21:26:26 +0200

    Update stg_products.sql

--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -1,2 +1,10 @@
-select *
-from {{ source('google_sheets','PRODUCTS') }}
+select
+
+    product_id,
+    product_name,
+    category,
+    price,
+
+    _fivetran_synced
+
+from {{ source('google_sheets', 'PRODUCTS') }}
