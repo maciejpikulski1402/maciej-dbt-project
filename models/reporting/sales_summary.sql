e6463875a48548e1b9d09ef43136b2913cef31d7
maciejpikulski1402 <maciej.pikulski1402@gmail.com>
2026-07-28 21:30:20 +0200

    Update sales_summary.sql

--- models/reporting/sales_summary.sql
+++ models/reporting/sales_summary.sql
@@ -1,3 +1,18 @@
 select
-    count(*) as total_sales
+
+    country,
+
+    category,
+
+    sum(sales_amount) as total_sales,
+
+    sum(quantity) as total_quantity,
+
+    count(*) as total_transactions
+
 from {{ ref('fct_sales') }}
+
+group by
+
+    country,
+    category
