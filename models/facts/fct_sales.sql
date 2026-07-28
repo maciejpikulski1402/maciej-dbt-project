0e9d74dbdfa3a2a94c4d833db7018b1659f336b9
maciejpikulski1402 <maciej.pikulski1402@gmail.com>
2026-07-28 21:29:26 +0200

    Update fct_sales.sql

--- models/facts/fct_sales.sql
+++ models/facts/fct_sales.sql
@@ -1,5 +1,25 @@
 select
 
-    *
+    s.sale_id,
+    s.sale_date,
 
-from {{ ref('stg_sales') }}
+    s.customer_id,
+    c.customer_name,
+    c.country,
+
+    s.product_id,
+    p.product_name,
+    p.category,
+
+    s.quantity,
+    p.price,
+
+    s.quantity * p.price as sales_amount
+
+from {{ ref('stg_sales') }} s
+
+left join {{ ref('dim_customer') }} c
+       on s.customer_id = c.customer_id
+
+left join {{ ref('dim_product') }} p
+       on s.product_id = p.product_id
